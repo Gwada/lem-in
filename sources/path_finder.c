0bb1b5f97d439863c846337dfab12ae4b4c00612
Dimitri LAVAURY-COLLOT <dlavaury@e3r10p18.42.fr>
2018-04-21 20:40:10 +0200

    end building graph

--- sources/path_finder.c
+++ sources/path_finder.c
@@ -20,59 +20,55 @@ static	void			first_step(t_graph *g)
 
 static	int				dijkstra(t_node *end, t_node *n, int ret)
 {
-	ft_printf("in dijkstra\n");
+//	ft_printf("in dijkstra\n");
 	if (!end || !n || !n->links || !n->n_links)
 		return (0);
 	if (n == end)
 		return (1);
-	ft_printf("node name = %s\tn->n_links = %d\n\n", n->name, n->n_links);
+//	ft_printf("node name = %s\tn->n_links = %d\n\n", n->name, n->n_links);
 	while (n->links && n->links->prev)
 		n->links = n->links->prev;
-	ft_printf("n->links->node->name = %s\n", n->links->node->name);
+//	ft_printf("n->links->node->name = %s\n", n->links->node->name);
 	n->bd |= VISITED;
 	while (n->links)
 	{
 		if (n->links->node->bd & FREE)
 			if (n->links->node->dis == -1 || n->dis + 1 < n->links->node->dis)
 			{
-				ft_printf("{yellow}-----------------------------------------------------------{eoc}\n");//
-				ft_printf("n->links->node->name = %s\n\n", n->links->node->name);
-				ft_printf("before\nn->links->node->dis = %d\n", n->links->node->dis);//
-				if (n->links->node->from)////////////////////////////////////////
-					ft_printf("n->links->node->from->name = %s\n\n", n->links->node->from->name);//
-				else/////////////////////////////////////////////////////////////
-					ft_printf("no parent\n\n");////////////////////////////////////
+//				ft_printf("{yellow}-----------------------------------------------------------{eoc}\n");//
+//				ft_printf("n->links->node->name = %s\n\n", n->links->node->name);
+//				ft_printf("before\nn->links->node->dis = %d\n", n->links->node->dis);//
+//				if (n->links->node->from)////////////////////////////////////////
+//					ft_printf("n->links->node->from->name = %s\n\n", n->links->node->from->name);//
+//				else/////////////////////////////////////////////////////////////
+//					ft_printf("no parent\n\n");////////////////////////////////////
 
 				n->links->node->dis = n->dis + 1;
 
-				ft_printf("after\nn->links->node->dis = %d\n", n->links->node->dis);//
+//				ft_printf("after\nn->links->node->dis = %d\n", n->links->node->dis);//
 
 				n->links->node->from = n;
 
-				if (n->links->node->from)////////////////////////////////////////
-					ft_printf("n->links->node->from->name = %s\n", n->links->node->from->name);//
-				ft_printf("{yellow}-----------------------------------------------------------{eoc}\n\n\n\n");//
+//				if (n->links->node->from)////////////////////////////////////////
+//					ft_printf("n->links->node->from->name = %s\n", n->links->node->from->name);//
+//				ft_printf("{yellow}-----------------------------------------------------------{eoc}\n\n\n\n");//
 			}
 		if (!n->links->next)
 			break;
 		n->links = n->links->next;
 	}
-//	return (0);
 	while (n->links && n->links->prev)
 		n->links = n->links->prev;
-	ft_printf("{red}test 1\n{eoc}");
 	while (n->links)
 	{
-//		ft_printf("{red}-----------------------------------------------------------\n");//
 		if (n->links->node->bd & FREE)
 			if (n->links->node->dis == -1 || n->dis + 1 <= n->links->node->dis)
+			//	n->links->node->bd ^ VISITED && dijkstra(end, n->links->node, 0) ? ret = 1 : 0;
 				dijkstra(end, n->links->node, 0) ? ret = 1 : 0;
 		if (!n->links->next)
 			break;
 		n->links = n->links->next;
 	}
-//		ft_printf("-----------------------------------------------------------{eoc}\n");//
-	ft_printf("\n");
 	return (ret);
 }
 
